2e7458788b155dd6da976bb65a799438cf91e517
FrederikBA <frederikbandersen98@gmail.com>
2024-04-18 08:56:12 +0200

    add failing unit tests

--- src/Chemicals.Core/Services/ProductService.cs
+++ src/Chemicals.Core/Services/ProductService.cs
@@ -1,10 +1,18 @@
 using Chemicals.Core.Entities.ChemicalAggregate;
 using Chemicals.Core.Interfaces.DomainServices;
+using Chemicals.Core.Interfaces.Repositories;
 
 namespace Chemicals.Core.Services;
 
 public class ProductService : IProductService
 {
+    private readonly IReadRepository<Product> _productReadRepository;
+
+    public ProductService(IReadRepository<Product> productReadRepository)
+    {
+        _productReadRepository = productReadRepository;
+    }
+
     public Task<List<Product>> GetAllProductsAsync()
     {
         throw new NotImplementedException();
